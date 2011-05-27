73e8930e2cba8c31ea3b9509d652783fa2a47a45
Shane Tomlinson <set117@yahoo.com>
2011-05-27 14:53:51 +0100

    Using setTimeout after returning from AJAX calls because jQuery catches any exceptions

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -1,11 +1,19 @@
 TWTU.Network = ( function() {
 	"use strict";
-	
+
 	var Network = {
 		ajax: function( config ) {
+			var success = config.success;
+			config.success = function( resp ) {
+				setTimeout( function() {
+					if( success ) {
+						success( resp );
+					}
+				}, 0 );
+			}
 			return $.ajax( config );
 		}
 	};
-	
+
 	return Network;
 }() );
