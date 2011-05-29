28d1e23e057768840fd3d68ad1f5cbffc9dc32e2
Shane Tomlinson <set117@yahoo.com>
2011-05-29 21:17:34 +0100

    Saving off the arguments the events were triggered with

--- client/test/helpers.js
+++ client/test/helpers.js
@@ -2,10 +2,13 @@
 var Events = {
 	events: {},
 	eventHandler: function( event ) {
-		Events.events[ event.type ] = true;
+		Events.events[ event.type ] = [].slice.call( arguments, 0 );
 	},
 	isTriggered: function( type ) {
 		return !!Events.events[ type ];
+	},
+	getArguments: function( type ) {
+		return Events.events[ type ];
 	},
 	reset: function() {
 		Events.events = {};
