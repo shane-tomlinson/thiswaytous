dda865837e61f611ae774123bb6d14bae9c66a21
Shane Tomlinson <set117@yahoo.com>
2012-06-03 21:36:06 +0100

    Cleanup

--- server.js
+++ server.js
@@ -1,79 +1,82 @@
-var sys = require( 'sys' ), 
-    http = require( 'http' ), 
-    fs = require( 'fs' ),
-	AFrame = require( './server/aframe-current-node' ), 
-    Sessions = require( './server/sessions' ),
-	sessions = Sessions.create(), 
-    Session = require( './server/session' ),
-	Users = require('./server/users'), 
-    sessionID = 0;
+const sys = require('sys'),
+      http = require('http'),
+      fs = require('fs'),
+      AFrame = require('./server/aframe-current-node'),
+      Sessions = require('./server/sessions'),
+      Session = require('./server/session'),
+      Users = require('./server/users'),
+      express = require('express'),
+      root = __dirname + "/",
+      PORT = 8000,
+      IP_ADDRESS = "0.0.0.0";
 
-var express = require( 'express' );
+var   sessions = Sessions.create(),
+      sessionID = 0,
+      app = express.createServer();
 
-var root = './';
+app.use(express.bodyParser());
 
-var app = express.createServer();
-app.use( express.bodyParser() );
+app.get('/client/(*)', function(req, res) {
+	sendFile('client/' + req.params[ 0 ], res);
+});
 
-app.get( '/client/(*)', function( req, res ) {
-	sendFile( 'client/' + req.params[ 0 ], res );
-} );
-app.get( '/', function( req, res ) {
-	sendFile( 'client/templates/index.mtp', res );
-} );
+app.get('/', function(req, res) {
+	sendFile('client/templates/index.mtp', res);
+});
 
-app.post( '/session/start', function( req, res ) {
+app.post('/session/start', function(req, res) {
     var body = req.body;
 	var session = sessions.createSession();
 
-    var user = JSON.parse( body.user );
-	session.addUpdateUser( user );
+    var user = JSON.parse(body.user);
+	session.addUpdateUser(user);
 
-    if( body.destination ) {
-        var dest = JSON.parse( body.destination );
-        session.setDestination( dest );
+    if(body.destination) {
+        var dest = JSON.parse(body.destination);
+        session.setDestination(dest);
     }
 
-    session.set( 'creator_id', user.id );
+    session.set('creator_id', user.id);
 
-	writeContentType( res, ContentTypes.json );
-	res.end( session.toString() );
-} );
+	writeContentType(res, ContentTypes.json);
+	res.end(session.toString());
+});
 
-app.post( '/session/join', joinUpdateUser );
-app.post( '/session/update', joinUpdateUser );
+app.post('/session/join', joinUpdateUser);
+app.post('/session/update', joinUpdateUser);
 
-app.listen( 8000 );
+console.log("Starting server on " + IP_ADDRESS + ":" + PORT);
+app.listen(PORT, IP_ADDRESS);
 
-function joinUpdateUser( req, res ) {
+function joinUpdateUser(req, res) {
 	var body = req.body;
-	var sessionData = JSON.parse( body.session );
-	var session = sessions.getSessionByData( sessionData );
+	var sessionData = JSON.parse(body.session);
+	var session = sessions.getSessionByData(sessionData);
 
-	if( session ) {
-	    var user = JSON.parse( body.user );
-		session.addUpdateUser( user );
+	if(session) {
+	    var user = JSON.parse(body.user);
+		session.addUpdateUser(user);
 
-        if( body.destination ) {
-            var dest = JSON.parse( body.destination );
-            session.setDestination( dest );
+        if(body.destination) {
+            var dest = JSON.parse(body.destination);
+            session.setDestination(dest);
         }
-		
-        writeContentType( res, ContentTypes.json );
-		res.end( session.toString() );
+
+        writeContentType(res, ContentTypes.json);
+		res.end(session.toString());
 	}
 	else {
-		console.log( 'session not found' );
-		res.writeHead( 404 );
+		console.log('session not found');
+		res.writeHead(404);
 		res.end();
 	}
 }
 
-function sendFile( fileName, res ) {
-	fs.readFile( root + fileName, function( err, data ) {
-		writeContentType( res, getContentTypes( fileName ) );
-		res.end( data );
-	} );
+function sendFile(fileName, res) {
+	fs.readFile(root + fileName, function(err, data) {
+		writeContentType(res, getContentTypes(fileName));
+		res.end(data);
+	});
 }
 
 var ContentTypes = {
@@ -84,14 +87,14 @@ var ContentTypes = {
 	'json': 'application/json'
 }
 
-function writeContentType( res, type ) {
-	res.writeHead( 200, {
+function writeContentType(res, type) {
+	res.writeHead(200, {
 		'Content-Type': type
-	} );
+	});
 }
 
-function getContentTypes( url ) {
-	var extension = url.match( /\.(\w+)$/ );
+function getContentTypes(url) {
+	var extension = url.match(/\.(\w+)$/);
 	var type = ContentTypes[ extension[ 1 ] ] || 'text/html';
 	return type;
 }
